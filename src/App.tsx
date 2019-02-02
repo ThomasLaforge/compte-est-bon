1ac6794a6a1362679e72ce8662911b83c84385ed
Thomas Laforge <thomas.laforge.38@gmail.com>
2019-02-02 16:31:48 +0100

    ui content

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import Problem from './components/Problem';
 import { ProblemBuilder } from './model/ProblemBuilder';
 import { Problem as ProblemClass } from './model/Problem'
 import { Resolver } from './model/Resolver';
 import { Solution } from './model/Solution';
 import { OPERATORS_LIST, Operator } from './defs';
 import { getStringFromOperator } from './utils';
+import Game from './components/Game';
 
 interface AppProps {
 }
@@ -44,47 +44,7 @@ class App extends Component<AppProps, AppState> {
     console.log('solution', this.state.solution, this.state.solution.toString(), this.state.solution.toStringArray())
     return (
       <div className="App">
-        <Problem object={this.state.problem} />
-        <div className="solution">
-      	  {this.state.solution.toStringArray().map( (line, i) => (
-            <div className="solution-line" key={i}>{line}</div>
-          ))}
-        </div>
-        <div className="player-zone">
-            <h2>Player</h2>
-            <div className="player-board">
-            
-            </div>
-            <div className="player-input">
-              <div className="player-input-number">
-                {this.state.problem.numbers.map( (n, i) => (
-                  <div className="player-numbers-elt" key={i}>
-                    <button onClick={() => this.handleClickOnNumber(n as number)}>{n}</button>
-                  </div>
-                ))}
-              </div>
-              <div className="player-input-operators">
-                {OPERATORS_LIST.map( (o, i) => (
-                  <div className="player-operator-elt" key={i}>
-                    <button>{getStringFromOperator(o)}</button>
-                  </div>
-                ))}
-              </div>
-              <div className="player-input-actions">
-                <button>{'<--'}</button>
-                <button>=</button>
-              </div>
-            </div>
-        </div>
-        <div className="solutions">
-          {/* {this.state.solutions.map( (s, i) => {
-            return s.toStringArray().map( (line, i) => (
-              <div className="solution-line" key={i}>{line}</div>
-            ))
-          })} */}
-          {/* {this.state.solutions.map( (s, i) => s.value + ',' + (i % 25 === 0 ? "\n" : ''))} */}
-          {this.state.solutions.length}
-        </div>
+        <Game />
       </div>
     );
   }
---
+++ src/components/Game.tsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react'
+import { Problem } from '../model/Problem';
+import { ProblemBuilder } from '../model/ProblemBuilder';
+import { Solution } from '../model/Solution';
+import { Step } from '../model/Step';
+import { OPERATORS_LIST } from '../defs';
+import { getStringFromOperator } from '../utils'
+
+interface GameProps {
+
+}
+
+interface GameState {
+    problem: Problem
+    solution?: Solution
+    renderSolution: boolean
+}
+
+export default class Game extends Component<GameProps, GameState> {
+    constructor(props: GameProps){
+        super(props)
+        this.state = {
+            problem: ProblemBuilder.generate(),
+            renderSolution: false
+        }
+    }
+
+
+    handleNumberClick(number: number, index: number){
+        console.log('click on number', number, index)
+    }
+    renderNumbers(){
+        return this.state.problem.numbers.map( (n, i) => (
+            <div 
+                className="numbers-elt" key={i}
+                onClick={() => this.handleNumberClick(n instanceof Step ? n.result : n, i)}
+            >{n}</div>
+        ))
+    }
+
+    renderSteps(){
+        return <div className="steps-elt">nothing for the moment ...</div>
+    }
+
+    renderOperators(){
+        return OPERATORS_LIST.map( (op, i) => (
+            <div className="operators-elt">{getStringFromOperator(op)}</div>
+        ))
+    }
+
+    render() {
+        return (
+            <div className='game'>
+                <div className="info-zone">
+                    <div className="objectif">
+                        {this.state.problem.expectedResult}
+                    </div>
+                    <div className="go-to-home-btn">Menu</div>
+                    <div className="new-game-btn">Nouvelle Partie</div>
+                    <div className="restart-btn">Recommencer</div>
+                    <div className="timer">90</div>
+                </div>
+                <div className="board">
+                    <div className="left-part">
+                        <div className="numbers">
+                            {this.renderNumbers()}
+                        </div>
+                        <div className="steps">
+                            {this.renderSteps()}
+                        </div>
+                        <div className="actions">
+                            <div className="delete-btn">Effacer</div>
+                            <div className="validate-btn">Valider</div>
+                        </div>
+                    </div>
+                    <div className="rigth-part">
+                        {this.renderOperators()}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
